c488c45819b1e095cb9154e39f4c14cda0aff8e4
charaekeow <charaekeow11@gmail.com>
2023-11-27 01:29:02 +0800

    FEAT: Add endpoint #2 to find odd-ending hash (Q2.2)
    The endpoint continously request hash from endpoint #1 until hash with odd number ending is found.

--- question2/index.js
+++ question2/index.js
@@ -21,6 +21,31 @@ function generateUniqueHash() {
   return hash;
 }
 
+/**
+ * Recursively requests a hash from another endpoint until finding a hash
+ * where the last character is an odd number.
+ *
+ * @returns {Object} An object indicating success when an odd-ending hash is found.
+ */
+async function requestOddHash() {
+  const response = await fetch(
+    `http://localhost:${PORT}/generate-unique-256-hash`
+  );
+  const { hash } = await response.json();
+
+  // get the last character
+  const lastChar = hash.slice(-1);
+
+  console.log({ hash, lastChar });
+
+  if (parseInt(lastChar) % 2 === 1) {
+    return { success: true };
+  } else {
+    // recursively call `requestOddHash` function until odd value is received
+    return await requestOddHash();
+  }
+}
+
 // routes
 app.get("/generate-unique-256-hash", (req, res) => {
   setTimeout(() => {
@@ -32,6 +57,17 @@ app.get("/generate-unique-256-hash", (req, res) => {
       res.status(500).send({ error: "Hash generation failed" });
     }
   }, DELAY);
+});
+
+app.get("/find-odd-ending-hash", async (req, res) => {
+  try {
+    const result = await requestOddHash();
+
+    res.status(200).send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Error requesting odd hash" });
+  }
 });
 
 app.listen(PORT, () => {
